828371bcfdb3f0eb35fd83adf312dce5a9f7b990
isaac <montesdeocamaravilla18@gmail.com>
2021-12-29 15:26:40 -0600

    mi primer commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Validar campos de formulario */
+function validar(){
+    var form = document.form;
+
+    if (form.name.value==0){
+        alert("El campo Nombre esta vacio");
+        form.name.value="";
+        form.name.focus();
+        return false;
+    }
+    if(form.email.value==0){
+        alert("el campo Email esta vacio");
+        form.email.value="";
+        form.email.focus();
+        return false;
+    }
+    if(form.asunto.value==0){
+        alert("el campo Asunto esta vacio");
+        form.asunto.value="";
+        form.asunto.focus();
+        return false;
+    }
+    if(form.mensaje.value==0){
+        alert("el campo Mensaje esta vacio");
+        form.mensaje.value="";
+        form.mensaje.focus();
+        return false;
+    }
+}
